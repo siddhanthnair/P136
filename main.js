4c065ffe57f4046c30f476586e14ef932a17bed6
siddhanthnair <72081663+siddhanthnair@users.noreply.github.com>
2023-01-16 16:30:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status= true
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status= true
+    video.loop;
+    video.speed(1);
+    video.volum(0);
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
